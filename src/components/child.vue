d016754cf5513a82db41149d0e732f507d63a166
lixiyan <lixiyan@xiyun.com.cn>
2019-07-29 15:16:14 +0800

    Initial commit

---
+++ src/components/child.vue
@@ -0,0 +1,46 @@
+<template>
+  <div >
+		子组件
+		<input type="text" v-model="nameWatch" >
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+	},
+  name: 'child',
+  data () {
+    return {
+      nameWatch:'',
+    }
+  },
+	mounted(){
+    this.nameWatch = this.name
+	},
+	watch: {
+    name(newVal){
+      this.nameWatch = this.name
+		},
+    nameWatch(newVal){
+      this.changeVal()
+		},
+    immediate: false
+	},
+	methods:{
+    changeVal(){
+      this.$emit('changeVal', this.nameWatch);
+    },
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/father.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		父组件
+		<input type="text" v-model="name"><br/>
+		<child :name="name" @changeVal="childVal"></child>
+	</div>
+</template>
+
+<script>
+  import Child from './child'
+
+  export default {
+    components: {Child},
+    name: 'father',
+    data() {
+      return {
+        name: 'father msg'
+      }
+    },
+		methods:{
+      childVal(data){
+        this.name=data
+			}
+		}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
